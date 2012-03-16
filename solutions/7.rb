89d649c3cf93a956ec58a784ec3a2fa3a35509c6
Miguel Marinhas <miguel.marinhas@gmail.com>
2012-03-16 17:04:51 +0000

    Faster solution

--- solutions/7.rb
+++ solutions/7.rb
@@ -3,7 +3,29 @@
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10001st prime number?
 =end
+
+inicio = Time.new
+
+def prime number
+  return true if number==2
+  max = Math.sqrt number
+  for i in 2..max
+    return false if number%i==0
+  end
+  return true
+end
+
+numto = 10001
+
 step = 0;
-num = 1;
-while(step!=10001)
-if (
+num = 2;
+while(step!=numto)
+  if prime(num)
+    step += 1
+    puts num
+  end
+  num += 1
+end
+
+final = Time.new
+puts "It took #{final-inicio}"
